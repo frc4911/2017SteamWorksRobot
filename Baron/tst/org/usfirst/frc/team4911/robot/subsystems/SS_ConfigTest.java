3133c7be33f647d3337b47c47ed122f304a15a70
ntroutman <nathanieltroutman@gmail.com>
2017-02-13 20:44:40 -0800

    Example Unit-Test for SS_Config Class

---
+++ Baron/tst/org/usfirst/frc/team4911/robot/subsystems/SS_ConfigTest.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team4911.robot.subsystems;
+
+import org.junit.Test;
+
+public class SS_ConfigTest extends SubsystemTestBase {
+
+	@Test
+	public void test() {
+		SS_Config config = new SS_Config();
+		// Config test code here
+	}
+
+}
