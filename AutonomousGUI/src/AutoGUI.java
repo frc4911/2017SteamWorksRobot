c4e3ea989c9a95cf95606ec4e46c7f9e3eae8de2
reddogfang <kingbros@msn.com>
2017-04-15 15:45:57 -0700

    scale for surface hi dpi

--- AutonomousGUI/src/AutoGUI.java
+++ AutonomousGUI/src/AutoGUI.java
@@ -15,9 +15,12 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.Date;
 import java.util.Random;
+import java.util.Set;
 
 import javax.swing.SwingConstants;
 import javax.swing.Timer;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
@@ -103,6 +106,37 @@ public class AutoGUI {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+				
+//				try {
+//					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//				} catch (ClassNotFoundException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				} catch (InstantiationException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				} catch (IllegalAccessException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				} catch (UnsupportedLookAndFeelException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
+//	            try {
+//	                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//	            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+//	                ex.printStackTrace();
+//	            }
+
+	            //setDefaultSize(24);
+//
+//	            JFrame frame = new JFrame("Testing frame");
+//	            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//	            frame.add(new JLabel("Hello world"));
+//	            frame.pack();
+//	            frame.setLocationRelativeTo(null);
+//	            frame.setVisible(true);
+				
 				try {
 					AutoGUI window = new AutoGUI();
 					window.frame.setVisible(true);
@@ -112,6 +146,27 @@ public class AutoGUI {
 			}
 		});
 	}
+	
+//	public static void setDefaultSize(int size) {
+//
+//	    Set<Object> keySet = UIManager.getLookAndFeelDefaults().keySet();
+//	    Object[] keys = keySet.toArray(new Object[keySet.size()]);
+//
+//	    for (Object key : keys) {
+//
+//	        if (key != null && key.toString().toLowerCase().contains("font")) {
+//
+//	            System.out.println(key);
+//	            Font font = UIManager.getDefaults().getFont(key);
+//	            if (font != null) {
+//	                font = font.deriveFont((float)size);
+//	                UIManager.put(key, font);
+//	            }
+//
+//	        }
+//
+//	    }
+//	}
 
 	/**
 	 * Create the application.
@@ -131,6 +186,12 @@ public class AutoGUI {
 		table = NetworkTable.getTable(tableName);
 	}
 
+	double scale = 1.5;
+
+	int c(int v){
+		
+		return (int)(v*scale);
+	}
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -141,89 +202,89 @@ public class AutoGUI {
 		for (int i=0; i<currentDesc.length; i++){
 			ct[i] = new CurrentTracker(currentDesc[i]); 
 		}	
-		
+				
 		// window size and position
 		frame = new JFrame();
-		frame.setBounds(0, 160, 780, 700);
+		frame.setBounds(0, c(160), c(780), c(700));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		int gap = 5;
+		int gap = c(5);
 		int theX = gap;
 		int theY = 0;
 		int theWidth = 0;
 		int theHeight = 0;
 
-		theWidth = 290;
-		theHeight = 72;
+		theWidth = c(290);
+		theHeight = c(72);
 	    JLabel lblTime = new JLabel("Match Time");
-	    lblTime.setFont(new Font("Tahoma", Font.PLAIN, 72));
+	    lblTime.setFont(new Font("Tahoma", Font.PLAIN, c(72)));
 	    lblTime.setHorizontalAlignment(SwingConstants.CENTER);
-	    lblTime.setBounds(theX, theY, theWidth+440, theHeight);
+	    lblTime.setBounds(theX, theY, theWidth+c(440), theHeight);
 	    frame.getContentPane().add(lblTime);
 		
-	    theY += theHeight + 5*gap;
-	    theHeight = 50;
+	    theY += theHeight + c(5)*gap;
+	    theHeight = c(50);
 		JLabel lblAutonomous = new JLabel("Autonomous");
 		lblAutonomous.setHorizontalAlignment(SwingConstants.LEFT);
 		lblAutonomous.setVerticalAlignment(SwingConstants.TOP);
-		lblAutonomous.setFont(new Font("Tahoma", Font.PLAIN, 48));
+		lblAutonomous.setFont(new Font("Tahoma", Font.PLAIN, c(48)));
 		lblAutonomous.setBounds(theX, theY, theWidth, theHeight);
 		frame.getContentPane().add(lblAutonomous);
 		
 	    theY += theHeight + gap;
-	    theHeight = 65;
+	    theHeight = c(65);
 		JTextPane textPane = new JTextPane();
-		textPane.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		textPane.setFont(new Font("Tahoma", Font.PLAIN, c(24)));
 		textPane.setBounds(theX, theY, theWidth, theHeight);
 		frame.getContentPane().add(textPane);
 		
 	    theY += theHeight + gap;
-	    theHeight = 35;
+	    theHeight = c(35);
 		JRadioButton tglbtnAuto_0 = new JRadioButton("Auto 0");
 		tglbtnAuto_0.setSelected(true);
-		tglbtnAuto_0.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		tglbtnAuto_0.setFont(new Font("Tahoma", Font.PLAIN, c(28)));
 		tglbtnAuto_0.setBounds(theX, theY, theWidth/2, theHeight);
 		frame.getContentPane().add(tglbtnAuto_0);
 
 		JRadioButton tglbtnAuto_5 = new JRadioButton("Auto 5");
-		tglbtnAuto_5.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		tglbtnAuto_5.setFont(new Font("Tahoma", Font.PLAIN, c(28)));
 		tglbtnAuto_5.setBounds(theX+theWidth/2, theY, theWidth/2, theHeight);
 		frame.getContentPane().add(tglbtnAuto_5);
 
 	    theY += theHeight + gap;
-	    theHeight = 35;
+	    theHeight = c(35);
 		JRadioButton tglbtnAuto_6 = new JRadioButton("Auto 6");
-		tglbtnAuto_6.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		tglbtnAuto_6.setFont(new Font("Tahoma", Font.PLAIN, c(28)));
 		tglbtnAuto_6.setBounds(theX, theY, theWidth/2, theHeight);
 		frame.getContentPane().add(tglbtnAuto_6);
 		
 		JRadioButton tglbtnAuto_7 = new JRadioButton("Auto 7");
-		tglbtnAuto_7.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		tglbtnAuto_7.setFont(new Font("Tahoma", Font.PLAIN, c(28)));
 		tglbtnAuto_7.setBounds(theX+theWidth/2, theY, theWidth/2, theHeight);
 		frame.getContentPane().add(tglbtnAuto_7);
 		
 	    theY += theHeight + gap;
-	    theHeight = 35;
+	    theHeight = c(35);
 		JRadioButton tglbtnAuto_30 = new JRadioButton("Auto 30");
-		tglbtnAuto_30.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		tglbtnAuto_30.setFont(new Font("Tahoma", Font.PLAIN, c(28)));
 		tglbtnAuto_30.setBounds(theX, theY, theWidth/2, theHeight);
 		frame.getContentPane().add(tglbtnAuto_30);
 		
 		JRadioButton tglbtnAuto_32 = new JRadioButton("Auto 32");
-		tglbtnAuto_32.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		tglbtnAuto_32.setFont(new Font("Tahoma", Font.PLAIN, c(28)));
 		tglbtnAuto_32.setBounds(theX+theWidth/2, theY, theWidth/2, theHeight);
 		frame.getContentPane().add(tglbtnAuto_32);
 
 	    theY += theHeight + gap;
-	    theHeight = 35;		
+	    theHeight = c(35);		
 		JRadioButton tglbtnAuto_40 = new JRadioButton("Auto 40");
-		tglbtnAuto_40.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		tglbtnAuto_40.setFont(new Font("Tahoma", Font.PLAIN, c(28)));
 		tglbtnAuto_40.setBounds(theX, theY, theWidth/2, theHeight);
 		frame.getContentPane().add(tglbtnAuto_40);
 		
 		JRadioButton tglbtnAuto_42 = new JRadioButton("Auto 42");
-		tglbtnAuto_42.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		tglbtnAuto_42.setFont(new Font("Tahoma", Font.PLAIN, c(28)));
 		tglbtnAuto_42.setBounds(theX+theWidth/2, theY, theWidth/2, theHeight);
 		frame.getContentPane().add(tglbtnAuto_42);
 				
@@ -240,9 +301,9 @@ public class AutoGUI {
 	    group.add(tglbtnAuto_42);
 	    
 	    theY += theHeight + 10*gap;
-	    theHeight = 52;
+	    theHeight = c(52);
 	    JLabel lblGearIn_1 = new JLabel("Gear Out");
-	    lblGearIn_1.setFont(new Font("Tahoma", Font.PLAIN, 28));
+	    lblGearIn_1.setFont(new Font("Tahoma", Font.PLAIN, c(28)));
 	    lblGearIn_1.setHorizontalAlignment(SwingConstants.CENTER);
 	    lblGearIn_1.setBounds(theX, theY, theWidth, theHeight);
 	    lblGearIn_1.setBackground(Color.RED);
@@ -250,7 +311,7 @@ public class AutoGUI {
 	    frame.getContentPane().add(lblGearIn_1);
 		
 	    theY += theHeight + 10*gap;
-	    theHeight = 52;
+	    theHeight = c(52);
 	    JButton btn = new JButton("Reset Stats");
 	    btn.setBounds(theX+theWidth/4, theY, theWidth/2, theHeight);
 	    btn.setVisible(true);
@@ -258,37 +319,37 @@ public class AutoGUI {
 	    frame.getContentPane().add(btn);
 	    
 	    // reset for 2nd column
-	    theY = 97;
-	    theX += 300+gap;
-	    theWidth = 440;
-	    theHeight = 50;
+	    theY = c(97);
+	    theX += c(300)+gap;
+	    theWidth = c(440);
+	    theHeight = c(50);
 		JLabel lblCurrents = new JLabel("Motor Current");
 		lblCurrents.setHorizontalAlignment(SwingConstants.LEFT);
 		lblCurrents.setVerticalAlignment(SwingConstants.TOP);
-		lblCurrents.setFont(new Font("Tahoma", Font.PLAIN, 48));
+		lblCurrents.setFont(new Font("Tahoma", Font.PLAIN, c(48)));
 		lblCurrents.setBounds(theX, theY, theWidth, theHeight);
 		frame.getContentPane().add(lblCurrents);
 		
 	    theY += theHeight + gap;
-	    theHeight = 340;
+	    theHeight = c(340);
 		JTextPane currentReport = new JTextPane();
-		currentReport.setFont(new Font("Courier New", Font.BOLD, 18));
+		currentReport.setFont(new Font("Courier New", Font.BOLD, c(18)));
 		currentReport.setBounds(theX, theY, theWidth, theHeight);
 		frame.getContentPane().add(currentReport);
 
 	    theY += theHeight + gap;
-	    theHeight = 54;
+	    theHeight = c(54);
 		JLabel lblEncoders = new JLabel("Encoders/Battery");
 		lblEncoders.setHorizontalAlignment(SwingConstants.LEFT);
 		lblEncoders.setVerticalAlignment(SwingConstants.TOP);
-		lblEncoders.setFont(new Font("Tahoma", Font.PLAIN, 48));
+		lblEncoders.setFont(new Font("Tahoma", Font.PLAIN, c(48)));
 		lblEncoders.setBounds(theX, theY, theWidth, theHeight);
 		frame.getContentPane().add(lblEncoders);
 		
 	    theY += theHeight + gap;
-	    theHeight = 90;
+	    theHeight = c(90);
 		JTextPane encodersReport = new JTextPane();
-		encodersReport.setFont(new Font("Courier New", Font.BOLD, 18));
+		encodersReport.setFont(new Font("Courier New", Font.BOLD, c(18)));
 		encodersReport.setBounds(theX, theY, theWidth, theHeight);
 		frame.getContentPane().add(encodersReport);
 
